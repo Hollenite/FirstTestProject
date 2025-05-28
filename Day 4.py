94a48c182161e59df98b06c0ac978e5cd472d0f1
Ariyan <ariyanpradhan112@gmail.com>
2025-05-28 12:01:42 +0530

    commit

--- Day 4.py
+++ Day 4.py
@@ -1,3 +1,14 @@
-print("Hello World")
-print("Bye world")
-print("Toy world")
+print("Welcome to Theme Park")
+age = input("What is Your Age?\n")
+
+age = int(age)
+if age >= 18:
+    print("You are old enough!")
+    camera = input("Would you like an extra camera?y/n")
+    if camera == "y":
+         print("Your bill is $20")
+    else:
+         print("Your bill is $18")
+else:
+     print("You are not old enough!")
+
