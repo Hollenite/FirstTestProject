2bb9144562da7367b9570b3d9cdfb01476cadf77
Ariyan <ariyanpradhan112@gmail.com>
2025-05-30 16:14:12 +0530

    added if user_input is not 0,1 or 2

--- rock paper scissor.py
+++ rock paper scissor.py
@@ -30,62 +30,60 @@ print("Press 0 for rock, 1 for paper, 2 for scissor")
 user_input = input()
 if user_input != "0" and user_input != "1" and user_input != "2":
     print("PLEASE CHOOSE A CORECT NUMBER!!!")
+else:
+    import random
+    random_number = random.randint(0, 2)
+    # if user_input == "0":
+    #     print("You chose rock!")
+    #     print(rock)
+    # elif user_input == "1":
+    #     print("You chose paper!")
+    #     print(paper)
+    # else:
+    #     print("You chose scissor!")
+    #     print(scissor)
+    #
+    # if random_number == 0:
+    #     print("I chose rock!")
+    #     print(rock)
+    # elif random_number == 1:
+    #     print("I chose paper!")
+    #     print(paper)
+    # else:
+    #     print("I chose scissor!")
+    #     print(scissor)
 
+    user_input2 = list2[int(user_input)]
+    user_input3 = list1[int(user_input)]
+    print(f'You chose {user_input2}')
+    print(user_input3)
 
-import random
-random_number = random.randint(0, 2)
-#
-# if user_input == "0":
-#     print("You chose rock!")
-#     print(rock)
-# elif user_input == "1":
-#     print("You chose paper!")
-#     print(paper)
-# else:
-#     print("You chose scissor!")
-#     print(scissor)
-#
-# if random_number == 0:
-#     print("I chose rock!")
-#     print(rock)
-# elif random_number == 1:
-#     print("I chose paper!")
-#     print(paper)
-# else:
-#     print("I chose scissor!")
-#     print(scissor)
+    random_number2 = list2[random_number]
+    random_number = list1[int(random_number)]
+    print(f'I choose {random_number2}')
+    print(random_number)
 
-user_input2 = list2[int(user_input)]
-user_input3 = list1[int(user_input)]
-print(f'You chose {user_input2}')
-print(user_input3)
-
-random_number2 = list2[random_number]
-random_number = list1[int(random_number)]
-print(f'I choose {random_number2}')
-print(random_number)
-
-if user_input3 == rock:
-        if random_number == paper:
-            print("You lose")
-        elif random_number == scissor:
-            print("You win")
-        elif random_number == rock:
-            print("Its a Draw")
-if user_input3 == paper:
-        if random_number == rock:
-            print("You win")
-        elif random_number == scissor:
-            print("You lose")
-        elif random_number == paper:
-            print("its a Draw")
-if user_input3 == scissor:
-        if random_number == rock:
-            print("You lose")
-        elif random_number == paper:
-            print("You win")
-        elif random_number == scissor:
-            print("Its a Draw")
+    if user_input3 == rock:
+            if random_number == paper:
+                print("You lose")
+            elif random_number == scissor:
+                print("You win")
+            elif random_number == rock:
+                print("Its a Draw")
+    if user_input3 == paper:
+            if random_number == rock:
+                print("You win")
+            elif random_number == scissor:
+                print("You lose")
+            elif random_number == paper:
+                print("its a Draw")
+    if user_input3 == scissor:
+            if random_number == rock:
+                print("You lose")
+            elif random_number == paper:
+                print("You win")
+            elif random_number == scissor:
+                print("Its a Draw")
 
 
 
